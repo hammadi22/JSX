6ac811cb4a57fcbeec6a08bb75ce149693b84b5f
hammadi22 <hammdi1996.houssem@gmail.com>
2021-03-07 20:08:07 +0100

    jsx-project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import video from './myVideo.mp4';
+import image from './imageInSrc.jpg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './style.css';
+
+class App extends Component {
+  render() {
+    const styles = {
+      border: '1px solid rgba(0, 0, 0, 0.05)', 
+ };
+    return (
+
+      <div className="App">
+      
+        <div style={styles}>
+          <h1 className = 'title red'>Houssem Eddine Hammadi</h1>
+          <br/>
+          <img src={image} width="320" height="240"  />
+          <br/>
+          <img src="../imageInPublic.jpg" width="320" height="240"  />
+        </div>
+        <video width="320" height="240" controls >
+        <source src={video} type="video/mp4"/>
+        </video>
+      </div>
+    );
+  }   
 }
 
 export default App;
